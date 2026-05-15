dec5e5da7dda322ab07f5b6806ac2c299dfea9cc
Alok Yadav <alokjar212@gmail.com>
2026-05-16 00:39:18 +0530

    basic server boilerplate code

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
-function greet(person:string):string{
-    return `Hello ${person}`
-}
+import 'dotenv/config';
+import express from "express";
+import cors from "cors";
+import connectDB from './db/db.js';
 
-const username:string = "Tony"
-console.log(greet(username))
+const app:express.Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+connectDB();
+
+app.get("/home",function(req,res){
+    res.json("Hello")
+})
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, ()=>{
+     console.log(`Server running on port ${PORT}`)
+})
