d85d55147458fbc6d3f574ab3685f9f7429d467f
Alok Yadav <alokjar212@gmail.com>
2026-05-17 00:29:12 +0530

    feat: added the route for pin and search feature

--- src/routes/note.route.ts
+++ src/routes/note.route.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { protect } from "../middlewares/auth.middleware.js";
-import { getAllNotes, createNote, getNoteById, updateNote, deleteNote, shareNote} from "../controllers/note.controller.js";
+import { getAllNotes, createNote, getNoteById, updateNote, deleteNote, shareNote, togglePin, searchNotes} from "../controllers/note.controller.js";
 
 const router = Router();
 
 router.use(protect);
 
-
+router.get("/search", searchNotes);
 
 router.get("/", getAllNotes);
  
@@ -25,6 +25,8 @@ router.delete("/:id", deleteNote);
 // POST /notes/:id/share
 router.post("/:id/share", shareNote);
 
+// PATCH /notes/:id/pin 
+router.patch("/:id/pin", togglePin);
 
 
 
