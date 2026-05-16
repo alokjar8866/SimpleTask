b5b2df58e43edeefb8c9bca9eecf9eafae38c4ff
Alok Yadav <alokjar212@gmail.com>
2026-05-17 00:23:14 +0530

    feat: added routes for CRUD

---
+++ src/routes/note.route.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+import { protect } from "../middlewares/auth.middleware.js";
+import { getAllNotes, createNote, getNoteById, updateNote, deleteNote} from "../controllers/note.controller.js";
+
+const router = Router();
+
+router.use(protect);
+
+
+
+router.get("/", getAllNotes);
+ 
+// POST /notes
+router.post("/", createNote);
+
+// GET  /notes/:id
+router.get("/:id", getNoteById);
+
+// PUT  /notes/:id
+router.put("/:id", updateNote);
+
+// DELETE /notes/:id
+router.delete("/:id", deleteNote);
+
+
+
+export default router;
