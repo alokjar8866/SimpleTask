3dd31168df05ccd018d7f8da503120432eb8289c
Alok Yadav <alokjar212@gmail.com>
2026-05-18 23:59:31 +0530

    fix: added validation middlewrae

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { login, register } from "../controllers/auth.controller.js";
+import { validate } from "../middlewares/validation.middleware.js";
+import { loginSchema, registerSchema } from "../schema/auth.schema.js";
 
 const router = Router();
 
-router.post("/register", register);
-router.post("/login", login);
+router.post("/register",validate(registerSchema), register);
+router.post("/login",validate(loginSchema), login);
  
 export default router;
  
